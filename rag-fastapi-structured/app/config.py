8917eb3c92ec3130851735257fcf799adf9b76e0
ch0992 <choilee6@naver.com>
2025-03-04 17:15:48 +0900

    update

--- rag-fastapi-structured/app/config.py
+++ rag-fastapi-structured/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    OLLAMA_BASE_URL: str = "http://localhost:11434"
+    OLLAMA_BASE_URL: str = "http://host.docker.internal:11434"
     MODEL_NAME: str = "deepseek-r1:8b"
     EMBEDDING_MODEL: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
     DOCUMENT_PATH: str = "./document"
